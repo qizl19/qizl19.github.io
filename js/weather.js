80658e0ab97b3f34548209375952c70663ea785f
“qizl19git config --global user.email qizl19@mails.tsinghua.edu.cngit config --global user.name “qizl19 <1073472023@qq.com>
2022-01-26 16:52:23 +0800

    Site updated: 2022-01-26 16:52:19

---
+++ js/weather.js
@@ -0,0 +1,25 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "5",
+        "tmpColor": "EDE4ED",
+        "tmpSize": "16",
+        "cityColor": "EDE4ED",
+        "citySize": "16",
+        "aqiColor": "EDE4ED",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "borderRadius": "5",
+        "fixed": "true",
+        "vertical": "top",
+        "horizontal": "left",
+        "left": "125",
+        "top": "10",
+        "key": "7ef2920c8b5a4ffb8b678df83bdd58b3",
+        "font": "Kaiti"
+    }
+}
